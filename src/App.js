32231f94703bfc6b2a709abb18749a83c0f881e7
Venkatesh <89681465+VenkateshwaranMarichamy@users.noreply.github.com>
2022-05-11 20:14:05 +0530

    todo basic commit with error

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import "./styles.css";
-
+import { useState } from "react";
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  function addToList(e) {
+    console.log("testing");
+    console.log(tasks);
+    // console.log(e);
+    setTasks(tasks.push(document.getElementById("task")));
+    console.log(tasks.map((element) => "element"));
+  }
+
+  function print() {
+    console.log(tasks);
+  }
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <label>Task</label>
+        <input type="text" name="task" id="task" />
+        <button onClick={addToList}>Add</button>
+      </div>
+      {print()}
     </div>
   );
 }
